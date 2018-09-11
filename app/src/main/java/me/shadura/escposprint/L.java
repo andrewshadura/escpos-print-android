b908b7a9fbcc6594a23c096c9d96dbd234643a64
Andrej Shadura <andrew@shadura.me>
2018-09-11 14:08:32 +0200

    Remove Crashlytics

--- app/src/main/java/me/shadura/escposprint/L.java
+++ app/src/main/java/me/shadura/escposprint/L.java
@@ -3,73 +3,65 @@ package me.shadura.escposprint;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.crashlytics.android.Crashlytics;
-
 /**
  * Logging/crash reporting functions
  */
 public class L {
     /**
-     * Verbose log + crashlytics log
+     * Verbose log
      *
      * @param msg Log message
      */
     public static void v(String msg) {
         Log.v(EscPosPrintApp.LOG_TAG, msg);
-        Crashlytics.log("V: " + msg);
     }
 
     /**
-     * Info log + crashlytics log
+     * Info log
      *
      * @param msg Log message
      */
     public static void i(String msg) {
         Log.i(EscPosPrintApp.LOG_TAG, msg);
-        Crashlytics.log("I: " + msg);
     }
 
     /**
-     * Warning log + crashlytics log
+     * Warning log
      *
      * @param msg Log message
      */
     public static void w(String msg) {
         Log.w(EscPosPrintApp.LOG_TAG, msg);
-        Crashlytics.log("W: " + msg);
     }
 
     /**
-     * Debug log + crashlytics log
+     * Debug log
      *
      * @param msg Log message
      */
     public static void d(String msg) {
         Log.d(EscPosPrintApp.LOG_TAG, msg);
-        Crashlytics.log("D: " + msg);
     }
 
     /**
-     * Error log + crashlytics log
+     * Error log
      *
      * @param msg Log message
      */
     public static void e(String msg) {
         Log.e(EscPosPrintApp.LOG_TAG, msg);
-        Crashlytics.log("E: " + msg);
     }
 
     /**
-     * Error reporting + send exception to crashlytics
+     * Error reporting
      *
      * @param msg Log message
-     * @param t   Throwable to send to crashlytics, if not null
+     * @param t   Throwable to print, if not null
      */
     public static void e(String msg, @Nullable Throwable t) {
         e(msg);
         if (t != null) {
             e(t.getLocalizedMessage());
-            Crashlytics.logException(t);
         }
     }
 }
