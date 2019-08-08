77ec1d79602c46a2e739438f24fe1d582612e44e
Andrej Shadura <andrew@shadura.me>
2019-08-08 07:21:31 +0200

    Add an invisible UsbEventReceiverActivity to allow binding to USB devices

---
+++ app/src/main/java/me/shadura/escposprint/printservice/UsbEventReceiverActivity.kt
@@ -0,0 +1,12 @@
+package me.shadura.escposprint.printservice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UsbEventReceiverActivity : AppCompatActivity() {
+
+    override fun onResume() {
+        super.onResume()
+        finish()
+    }
+}
