7ae70b6aa8b93a5727541cd37657b566a26e0bb0
Andrej Shadura <andrew@shadura.me>
2019-10-18 21:42:52 +0200

    Verbose logging in debug builds

--- app/src/main/java/me/shadura/escposprint/EscPosPrintApp.kt
+++ app/src/main/java/me/shadura/escposprint/EscPosPrintApp.kt
@@ -1,6 +1,7 @@
 package me.shadura.escposprint
 
 import android.content.Context
+import android.util.Log
 import androidx.multidex.MultiDexApplication
 
 import com.hypertrack.hyperlog.HyperLog
@@ -12,6 +13,7 @@ class EscPosPrintApp : MultiDexApplication() {
         instance = this
 
         HyperLog.initialize(this)
+        HyperLog.setLogLevel(if (BuildConfig.DEBUG) Log.DEBUG else Log.WARN)
     }
 
     companion object {
